4a405948ac1d89c1e1bfc0afb0f5d55c3ef73c4e
Gloria Villa <hello.gloria@gmail.com>
2024-01-14 18:22:12 -0800

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Gloria Villa on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
